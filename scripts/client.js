dd1f7be43b15c4c46ad893c345f7bfd863722b0e
peterorbb <orbitalist@gmail.com>
2017-05-19 15:32:33 +0300

    May 19 update 4

--- scripts/client.js
+++ scripts/client.js
@@ -1,4 +1,14 @@
 $(document).ready(
     function () {
 
+        header();
+        footer();
     });
+
+function header () {
+    $('header').load('./views/components/header.html');
+}
+
+function footer () {
+    $('footer').load('./views/components/footer.html');
+}
