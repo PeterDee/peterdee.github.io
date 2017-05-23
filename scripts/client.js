c140a6345b10bd0bfce827c20c6e8156d326fc0a
peterorbb <orbitalist@gmail.com>
2017-05-23 17:09:18 +0300

    May 23 update 1

--- scripts/client.js
+++ scripts/client.js
@@ -4,6 +4,10 @@ $(document).ready(
         main();
         footer();
 
+        $('#header-link-home').on('click', main);
+        $('#header-link-about').on('click', about);
+        $('#header-link-links').on('click', links);
+
         $('#footer-link-home').on('click', main);
     });
 
@@ -17,4 +21,21 @@ function footer () {
 
 function main () {
     $('#main').load('./views/pages/main.html');
+    $('#navHome').addClass('header__nav_selected');
+    $('#navAbout').removeClass('header__nav_selected');
+    $('#navLinks').removeClass('header__nav_selected');
+}
+
+function about () {
+    $('#main').load('./views/pages/about.html');
+    $('#navAbout').addClass('header__nav_selected');
+    $('#navHome').removeClass('header__nav_selected');
+    $('#navLinks').removeClass('header__nav_selected');
+}
+
+function links () {
+    $('#main').load('./views/pages/links.html');
+    $('#navLinks').addClass('header__nav_selected');
+    $('#navAbout').removeClass('header__nav_selected');
+    $('#navHome').removeClass('header__nav_selected');
 }
