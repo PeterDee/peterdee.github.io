75311e77ff38e28695b629bd101ffa9e2239a598
peterorbb <orbitalist@gmail.com>
2019-09-28 00:35:38 +0300

    Style fixes, rebuild

--- precache-manifest.8345335a55e9f12ea94d04abfde5ee0b.js
+++ precache-manifest.8345335a55e9f12ea94d04abfde5ee0b.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "330f685f6036d70d40c9b166301d1053",
+    "revision": "356d240f98c982e214d410e8f64085ab",
     "url": "/index.html"
   },
   {
-    "revision": "6339d366c8db078cd613",
-    "url": "/static/css/main.39c676d7.chunk.css"
+    "revision": "cde685b3dbfc5ed0d6cb",
+    "url": "/static/css/main.9d6f28a0.chunk.css"
   },
   {
     "revision": "2b2d8cfae4410046447f",
     "url": "/static/js/2.abf9eb48.chunk.js"
   },
   {
-    "revision": "6339d366c8db078cd613",
+    "revision": "cde685b3dbfc5ed0d6cb",
     "url": "/static/js/main.62cd8723.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.34934c14684f9be81b66e7455cb99d20.js"
+  "/precache-manifest.8345335a55e9f12ea94d04abfde5ee0b.js"
 );
 
 self.addEventListener('message', (event) => {
